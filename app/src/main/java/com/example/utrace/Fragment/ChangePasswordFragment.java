f202a1e45b42424791b351b35832e929b4065db2
Alberto Novello <albertonovello03@gmail.com>
2024-12-14 17:50:11 +0100

    change data fragments done
    Fragments done, they are not connected to anything ( i didn't know how how you wanted them to be launched =/  )

---
+++ app/src/main/java/com/example/utrace/Fragment/ChangePasswordFragment.java
@@ -0,0 +1,33 @@
+package com.example.utrace.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.utrace.R;
+
+
+public class ChangePasswordFragment extends Fragment {
+
+    public static ChangePasswordFragment newInstance(String param1, String param2) {
+        ChangePasswordFragment fragment = new ChangePasswordFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_change_password, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/utrace/Fragment/ChangeUserFragment.java
@@ -0,0 +1,34 @@
+package com.example.utrace.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.utrace.R;
+
+
+public class ChangeUserFragment extends Fragment {
+
+    public static ChangeUserFragment newInstance(String param1, String param2) {
+        ChangeUserFragment fragment = new ChangeUserFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_change_user, container, false);
+    }
+}
